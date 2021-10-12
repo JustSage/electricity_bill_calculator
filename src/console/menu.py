c96aea81a442b44866a4e6b1856d1f58384f20b2
Sage Baram <sagebaram@gamil.com>
2021-10-12 20:45:30 +0300

    feat(menu): add a previous bill listing command to the menu; wrapped exception clauses
    on top of making the menu run infinitely, a quit option has been added. added another command, and
    wrapped user input in exception clauses for value errors.

--- src/console/menu.py
+++ src/console/menu.py
@@ -1,16 +1,25 @@
-from utils.utils import clean_function_name
+from utils.utils import clean_function_name, list_previous_bills
 from bills.elec_bill import calculate_my_electric_bill
 
 def menu():
+    commands = { 1 : calculate_my_electric_bill, 2 : list_previous_bills}
     print("_______ WELCOME _______")
     print("Function Menu: ")
-    commands = {"1": calculate_my_electric_bill}
 
-    for key, val in commands.items():
-        name = clean_function_name(val)
-        print(key + ") " + name)
-    user_input = str(input("Enter Number From Menu: "))
-    cmd = commands[user_input]
-    cmd()
+    while True:
+        for key, val in commands.items():
+            name = clean_function_name(val)
+            print(str(key) + ") " + name)
+        print("Enter Q to quit")
+        try:
+            user_input = input("Enter menu option: ")
+            if user_input in ['Q','q']:
+                print("Have a good day!")
+                exit()
+            if int(user_input) in commands.keys():
+                print()
+                cmd = commands[int(user_input)]
+                cmd()
 
-
+        except ValueError:
+            print("Invalid Input")
